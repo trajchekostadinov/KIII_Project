10b45659aa37416c96862ac840e8f30b6c16c1a6
trajchekostadinov <kostadinovtrajce3@gmail.com>
2026-05-05 12:29:25 +0200

    Initial commit for Invoices-Stripe Project

---
+++ src/main/java/com/example/invoicesstripe/InvoicesStripeApplication.java
@@ -0,0 +1,12 @@
+package com.example.invoicesstripe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicesStripeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoicesStripeApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/invoicesstripe/InvoicesStripeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.invoicesstripe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicesStripeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
